f5f661b3dd20b65a82d85f8f8abedbf5e809f399
Matthew Grinnell <matthew.grinnell@dfo-mpo.gc.ca>
2020-02-25 12:14:21 -0800

    Describe width factors

--- R/data.R
+++ R/data.R
@@ -1,11 +1,3 @@
-#' Example Pacific Herring spawn survey database.
-#'
-#' Example Pacific Herring spawn survey database. This is a subset of the
-#' actual spawn survey database, and some additional data so that examples run.
-#'
-#' @format MS Access databases.
-# "HerringSpawn.mdb"
-
 #' Understory spawn width correction factors.
 #'
 #' Spawn width correction factors for Pacific Herring understory spawn surveys
@@ -13,4 +5,16 @@
 #'
 #' @format Tibble with 12 rows and 8 columns (one for year, and one for each
 #' regions).
-#' underWidthFac
+#' @note Adjust spawn width for affected understory spawn surveys by multiplying
+#'   the observed width by the corresponding correction fator for the affected
+#'   years.
+#' @seealso \code{\link{CalcUnderSpawn}}
+"underWidthFac"
+
+#' Example Pacific Herring spawn survey database.
+#'
+#' Example Pacific Herring spawn survey database. This is a subset of the
+#' actual spawn survey database, and some additional data so that examples run.
+#'
+#' @format MS Access databases.
+# "HerringSpawn.mdb"
