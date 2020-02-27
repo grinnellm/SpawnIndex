c504309ac09a7bbda0b6e72c10f29895ca45d468
Matthew Grinnell <matthew.grinnell@dfo-mpo.gc.ca>
2020-02-27 11:23:52 -0800

    Clean folder

---
+++ doc/Fresh.bat
@@ -0,0 +1,20 @@
+# Clean up the directory after a LaTeX build (Windows version)
+del *.aux
+del *.bbl
+del *.blg
+del *.dvi
+del *.log
+del *.lof
+del *.lot
+del *.nav
+del *.out
+del *.ps
+del *.snm
+del *.toc
+del *.vrb
+del *-concordance.tex
+del *.synctex.gz*
+del SpawnIndex.tex
+del SpawnIndex.pdf
+rmdir /S /Q knitr-cache
+rmdir /S /Q knitr-figs
