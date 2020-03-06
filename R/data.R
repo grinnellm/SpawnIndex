9d02cba632c7b0fc09de97bcdaaff5329f72b91e
Matthew Grinnell <matthew.grinnell@dfo-mpo.gc.ca>
2020-03-06 11:49:54 -0800

    Move mdb documentation to text file (not working)

--- R/data.R
+++ R/data.R
@@ -98,36 +98,3 @@
 #' data(algaeCoefs)
 #' algaeCoefs
 "algaeCoefs"
-
-#' Example Pacific Herring spawn survey database.
-#'
-#' Example Pacific Herring spawn survey database. This database contains a
-#' subset of tables from the spawn survey database, and some additional tables
-#' from other databases to ensure the examples work.
-#'
-#' @format MS Access databases with tables:
-#' \describe{
-#'   \item{Location}{Spatial information for Locations.}
-#'   \item{Sections}{Spatial information for Sections.}
-#'   \item{PoolStd}{Spawn width for Beds.}
-#'   \item{SectionStd}{Spawn width for Sections.}
-#'   \item{RegionStd}{Spawn width for Regions.}
-#'   \item{tSSAllspawn}{Spawn survey information.}
-#'   \item{tSSMacPlant}{Macrocystis plant information.}
-#'   \item{tSSMacTrans}{Macrocystis transect information.}
-#'   \item{tSSStations}{Understory quadrat information.}
-#'   \item{tSSSurface}{Surface spawn information}
-#'   \item{tSSVegetation}{Understory spawn algae information.}
-#'   \item{tSSVegTrans}{Understory spawn transect information.}
-#' }
-#' @docType data
-#' @seealso \code{\link{LoadAreaData}} \code{\link{CalcSurfSpawn}}
-#'   \code{\link{CalcMacroSpawn}} \code{\link{CalcUnderSpawn}}
-#' @note This is a 32-bit MS Access database, and it requires 32-bit R to access
-#'   the data. In addition, MS Windows is required to access the data using the
-#'   RODBC package. This example database only includes WCVI from 2010 to 2015.
-#'
-system.file("extdata", "HerringSpawn.mdb",
-  package = "SpawnIndex",
-  mustWork = TRUE
-)
