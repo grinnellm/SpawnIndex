268391c95faf975bf250fb06d604f6cd995869ae
Grinnell <Matthew.Grinnell@dfo-mpo.gc.ca>
2021-01-07 09:29:49 -0800

    Minor

--- sticker/sticker.R
+++ sticker/sticker.R
@@ -2,8 +2,8 @@ hImage <- file.path("sticker", "HerringColourDFO.jpg")
 siURL <- "https://github.com/grinnellm/SpawnIndex"
 stickerSI <- sticker(subplot = hImage, package = "SpawnIndex", p_size = 20,
                      s_x = 1, s_y = 0.8, s_width = 0.9, h_fill = "white",
-                     p_color = "red", h_color = "black", h_size = 6,
-                     spotlight = TRUE, dpi = 600, u_size = nchar(siURL)/11,
-                     url = siURL, white_around_sticker = TRUE,
+                     p_color = "red", h_color = "red", h_size = 4,
+                     spotlight = TRUE, dpi = 300, u_size = nchar(siURL)/11,
+                     url = siURL,
                      filename = file.path("sticker", "SpawnIndex-sticker.png"))
 print(stickerSI)
