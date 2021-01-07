776ead0bd151d058354d61abb131609f9c2f6716
Grinnell <Matthew.Grinnell@dfo-mpo.gc.ca>
2021-01-07 13:56:10 -0800

    Done sticker!

--- sticker/sticker.R
+++ sticker/sticker.R
@@ -9,10 +9,10 @@ siURL <- "https://github.com/grinnellm/SpawnIndex"
 
 # Make the sticker
 stickerSI <- sticker(subplot = hImage, package = "SpawnIndex", p_size = 12,
-                     p_y = 1.6,
-                     s_x = 1, s_y = 0.9, s_width = 0.5, h_fill = "white",
-                     p_color = "red", h_color = "red", h_size = 3,
-                     dpi = 300, u_size = nchar(siURL)/11, url = siURL,
+                     p_y = 1.6, s_x = 1, s_y = 0.9, s_width = 0.5,
+                     h_fill = "white", p_color = "red", h_color = "red",
+                     h_size = 1, dpi = 300, u_size = nchar(siURL)/10.5,
+                     url = siURL,
                      filename = file.path("sticker", "sticker.png"))
 
 # Display the sticker
