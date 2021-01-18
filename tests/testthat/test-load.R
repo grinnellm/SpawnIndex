789663a25b43710c091ca5f1a08f34849d0fdaab
Grinnell <Matthew.Grinnell@dfo-mpo.gc.ca>
2021-01-18 11:03:15 -0800

    Fix load area data

--- tests/testthat/test-load.R
+++ tests/testthat/test-load.R
@@ -1,48 +1,26 @@
 context("load.R")
 
-# test_that("Load area data", {
-#   expect_type(
-#     {
-#       dbLoc <- system.file("extdata", package = "SpawnIndex")
-#       areaLoc <- list(
-#         loc = dbLoc, db = "HerringSpawn.mdb",
-#         fns = list(sections = "Sections", locations = "Location")
-#       )
-#       areas <- LoadAreaData(reg = "WCVI", where = areaLoc, quiet = TRUE)
-#       areas
-#     },
-#     "list"
-#   )
-#   expect_equal(
-#     {
-#       dbLoc <- system.file("extdata", package = "SpawnIndex")
-#       areaLoc <- list(
-#         loc = dbLoc, db = "HerringSpawn.mdb",
-#         fns = list(sections = "Sections", locations = "Location")
-#       )
-#       areas <- LoadAreaData(reg = "WCVI", where = areaLoc, quiet = TRUE)
-#       ncol(areas)
-#     },
-#     13
-#   )
-# })
-
-# test_that("Load allSpawn", {
-#   expect_is(
-#     {
-#       dbLoc <- system.file("extdata", package = "SpawnIndex")
-#       areaLoc <- list(
-#         loc = dbLoc, db = "HerringSpawn.mdb",
-#         fns = list(sections = "Sections", locations = "Location")
-#       )
-#       areas <- LoadAreaData(reg = "WCVI", where = areaLoc, quiet = TRUE)
-#       allSpawnLoc <- list(
-#         loc = dbLoc, db = "HerringSpawn.mdb",
-#         fns = list(allSpawn = "tSSAllspawn", stations = "tSSStations")
-#       )
-#       allSpawn <- LoadAllSpawn(where = allSpawnLoc, a = areas, yrs = 2010:2015)
-#       allSpawn
-#     },
-#     "data.frame"
-#   )
-# })
+test_that("Load area data", {
+  expect_type(
+    {
+      LoadAreaData(reg = "WCVI", where = list(
+        loc = system.file("extdata", package = "SpawnIndex"),
+        db = "HerringSpawn.mdb",
+        fns = list(sections = "Sections", locations = "Location")
+      ), quiet = TRUE)
+    },
+    "list"
+  )
+  expect_named(
+    {
+      LoadAreaData(reg = "WCVI", where = list(
+        loc = system.file("extdata", package = "SpawnIndex"),
+        db = "HerringSpawn.mdb",
+        fns = list(sections = "Sections", locations = "Location")
+      ), quiet = TRUE)
+    },
+    c("SAR", "Region", "RegionName", "StatArea", "Group", "Section",
+      "LocationCode", "LocationName", "Pool", "Eastings", "Northings",
+      "Longitude", "Latitude")
+  )
+})
