16db959effd99a2e742f90f966752a4eb6b22954
Grinnell <Matthew.Grinnell@dfo-mpo.gc.ca>
2021-04-16 13:25:17 -0700

    Create make.R for pre-check checks

---
+++ make.R
@@ -0,0 +1,25 @@
+# Additional checks to run before R-CMD-CHECK
+
+# Build the raw data files
+source(file.path("data-raw", "pars.R"))
+
+# Build the read me file
+rmarkdown::render("README.Rmd")
+
+# Knit the technical report
+setwd("tr")
+knitr::knit2pdf("SpawnIndex.Rnw")
+setwd("..")
+
+# Styler
+styler::style_pkg()
+styler::style_file(file.path("vignettes", "Introduction.Rmd"))
+styler::style_file(file.path("tr", "SpawnIndex.Rnw"))
+
+# Lint
+lintr::lint_package()
+lintr::lint(file.path("vignettes", "Introduction.Rmd"))
+lintr::lint(file.path("tr", "SpawnIndex.Rnw"))
+
+# Good practice (takes a while)
+goodpractice::gp()
