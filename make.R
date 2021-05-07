931b8dbb4fed083e2fe0538b59e572ebeb223a26
Grinnell <Matthew.Grinnell@dfo-mpo.gc.ca>
2021-05-07 10:48:22 -0700

    Argument names

--- make.R
+++ make.R
@@ -1,29 +1,31 @@
 # Additional checks to run before R-CMD-CHECK
 
 # Build the raw data files
-source(file.path("data-raw", "pars.R"))
+source(file = file.path("data-raw", "pars.R"))
 
 # Build the read me file
-rmarkdown::render("README.Rmd")
+rmarkdown::render(input = "README.Rmd")
 file.remove("README.html")
 
 # Knit the technical report
-setwd("tr")
-knitr::knit2pdf("SpawnIndex.Rnw")
-setwd("..")
+setwd(dir = "tr")
+knitr::knit2pdf(input = "SpawnIndex.Rnw")
+setwd(dir = "..")
 
 # Compile the manual
 devtools::build_manual(path = "./doc")
 
 # Styler
 styler::style_pkg()
-styler::style_file(file.path("vignettes", "Introduction.Rmd"))
-# styler::style_file(file.path("tr", "SpawnIndex.Rnw"))
+styler::style_file(path = file.path("vignettes", "Introduction.Rmd"))
+styler::style_file(path = "README.Rmd")
+# styler::style_file(path = file.path("tr", "SpawnIndex.Rnw"))
 
 # Lint
 lintr::lint_package()
-lintr::lint(file.path("vignettes", "Introduction.Rmd"))
-lintr::lint(file.path("tr", "SpawnIndex.Rnw"))
+lintr::lint(filename = file.path("vignettes", "Introduction.Rmd"))
+lintr::lint(filename = "README.Rmd")
+lintr::lint(filename = file.path("tr", "SpawnIndex.Rnw"))
 
 # Good practice (takes a while)
-goodpractice::gp()
+goodpractice::gp(path = ".")
