f0c845f7b54ebb4933b461e32d328f359afd67f3
Grinnell <Matthew.Grinnell@dfo-mpo.gc.ca>
2021-06-01 12:14:48 -0700

    Update mans

--- man-roxygen/param-areas.R
+++ man-roxygen/param-areas.R
@@ -1,3 +1,2 @@
 #' @param areas Tibble. Table of geographic information indicating the subset of
-#'   spawn survey observations to include in calculations; from
-#'   \code{\link{load_area_data}}.
+#'   observations to include; from \code{\link{load_area_data}}.
--- man-roxygen/param-sec_sub.R
+++
@@ -1,2 +0,0 @@
-#' @param sec_sub Numeric vector or NULL. Subset of Sections to include in the
-#'   analysis, or NULL to include all the Sections in the region.
