d7ae9e45ce1dcac2cf260c134119f560af0ddeea
Grinnell <Matthew.Grinnell@dfo-mpo.gc.ca>
2021-06-02 13:53:43 -0700

    Remove commented code

--- tests/testthat/test-load.R
+++ tests/testthat/test-load.R
@@ -1,30 +1,3 @@
-# test_that("Load area data", {
-#   expect_type(
-#     {
-#       load_area_data(reg = "WCVI", where = list(
-#         loc = system.file("extdata", package = "SpawnIndex"),
-#         db = "HerringSpawn.mdb",
-#         fns = list(sections = "Sections", locations = "Location")
-#       ), quiet = TRUE)
-#     },
-#     "list"
-#   )
-#   expect_named(
-#     {
-#       load_area_data(reg = "WCVI", where = list(
-#         loc = system.file("extdata", package = "SpawnIndex"),
-#         db = "HerringSpawn.mdb",
-#         fns = list(sections = "Sections", locations = "Location")
-#       ), quiet = TRUE)
-#     },
-#     c(
-#       "SAR", "Region", "RegionName", "StatArea", "Group", "Section",
-#       "LocationCode", "LocationName", "Pool", "Eastings", "Northings",
-#       "Longitude", "Latitude"
-#     )
-#   )
-# })
-
 test_that("Load sections", {
   library(sf)
   db_loc <- system.file("extdata", package = "SpawnIndex")
