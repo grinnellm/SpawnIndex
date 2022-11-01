eb727360396a162e1494a5bf84332a0072e2c9d4
Grinnell <Matthew.Grinnell@dfo-mpo.gc.ca>
2022-11-01 13:10:24 -0700

    Use here::here

--- make.R
+++ make.R
@@ -2,37 +2,36 @@
 
 # Load packages
 require(SpawnIndex)
-require(here)
 
 # Build the raw data files
-source(file = here("data-raw", "pars.R"))
+source(file = here::here("data-raw", "pars.R"))
 
 # Build the read me file
-rmarkdown::render(input = here("README.Rmd"))
-file.remove(here("README.html"))
+rmarkdown::render(input = here::here("README.Rmd"))
+file.remove(here::here("README.html"))
 
 # Knit the technical report
-setwd(dir = here("tr"))
+setwd(dir = here::here("tr"))
 knitr::knit2pdf(input = "SpawnIndex.Rnw")
-setwd(dir = here())
+setwd(dir = here::here())
 
 # Compile the supporting documents
-devtools::build_manual(path = here("doc"))
+devtools::build_manual(path = here::here("doc"))
 devtools::build_vignettes(pkg = ".")
 
 # Styler
 styler::style_pkg()
-styler::style_file(path = here("vignettes", "Introduction.Rmd"))
-styler::style_file(path = here("man", "sticker", "sticker.R"))
-styler::style_file(path = here("README.Rmd"))
-styler::style_file(path = here("tr", "SpawnIndex.Rnw"))
+styler::style_file(path = here::here("vignettes", "Introduction.Rmd"))
+styler::style_file(path = here::here("man", "sticker", "sticker.R"))
+styler::style_file(path = here::here("README.Rmd"))
+styler::style_file(path = here::here("tr", "SpawnIndex.Rnw"))
 
 # Lint
 lintr::lint_package()
-lintr::lint(filename = here("vignettes", "Introduction.Rmd"))
-lintr::lint(filename = here("man", "sticker", "sticker.R"))
-lintr::lint(filename = here("README.Rmd"))
-lintr::lint(filename = here("tr", "SpawnIndex.Rnw"))
+lintr::lint(filename = here::here("vignettes", "Introduction.Rmd"))
+lintr::lint(filename = here::here("man", "sticker", "sticker.R"))
+lintr::lint(filename = here::here("README.Rmd"))
+lintr::lint(filename = here::here("tr", "SpawnIndex.Rnw"))
 
 # Good practice (takes a while; restart R and require `SpawnIndex` first)
 goodpractice::gp(path = ".")
